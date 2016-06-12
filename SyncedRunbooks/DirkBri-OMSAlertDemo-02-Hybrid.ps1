b50334ff77dd91a606437f2acd1d56f40a98b0e8
Dirk Brinkmann <DirkBrinkmann@users.noreply.github.com>
2016-06-12 14:31:39 +0200

    Created DirkBri-OMSAlertDemo-02-Hybrid.ps1 in AzureAutomation.

---
+++ SyncedRunbooks/DirkBri-OMSAlertDemo-02-Hybrid.ps1
@@ -0,0 +1,21 @@
+param ([Object]$Webhookdata)
+
+[String]$Reportfolder="C:\Temp"
+
+if ($Webhookdata -ne $null)
+{
+	$WebhookBody = convertfrom-json $Webhookdata.RequestBody
+
+	foreach ($Result in $WebhookBody.SearchResults.value)
+	{
+		$FileName = "{0:yyyy-MM-dd_HH-mm-ss}_{1}_SearchResult.txt" -f (get-date), $Result.ID
+		$FilePath = join-path $Reportfolder $FileName	
+		$Result | out-file $FilePath
+		
+	}
+}
+else
+{
+	write-output "No Webhookdata received"
+	
+}
