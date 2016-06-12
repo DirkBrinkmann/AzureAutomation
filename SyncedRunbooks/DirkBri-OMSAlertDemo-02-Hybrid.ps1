d8fd716680f7f1f4195bf2c33dffeb36ef413eb5
Dirk Brinkmann <DirkBrinkmann@users.noreply.github.com>
2016-06-12 14:34:11 +0200

    Updated DirkBri-OMSAlertDemo-02-Hybrid.ps1 in Azure Automation.

--- SyncedRunbooks/DirkBri-OMSAlertDemo-02-Hybrid.ps1
+++ SyncedRunbooks/DirkBri-OMSAlertDemo-02-Hybrid.ps1
@@ -11,6 +11,8 @@ if ($Webhookdata -ne $null)
 		$FileName = "{0:yyyy-MM-dd_HH-mm-ss}_{1}_SearchResult.txt" -f (get-date), $Result.ID
 		$FilePath = join-path $Reportfolder $FileName	
 		$Result | out-file $FilePath
+		$strMessage = "Written file to {0}" -f $FilePath
+		write-output $strMessage
 		
 	}
 }
