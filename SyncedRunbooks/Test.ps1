b2272a590a9017645a21cd666be45e7935a87b7a
Dirk Brinkmann <DirkBrinkmann@users.noreply.github.com>
2016-07-14 14:31:48 +0200

    Updated Test.ps1 in Azure Automation.

--- SyncedRunbooks/Test.ps1
+++ SyncedRunbooks/Test.ps1
@@ -1,4 +1,5 @@
-"Hallo Welt" | out-file c:\temp\azure.txt
-start-sleep -seconds 10
+$strMessage = "{0} Hallo Welt" -f (get-date)
+$strMessage | out-file c:\temp\azure.txt
+#start-sleep -seconds 10
 
 #Modified 14.07.2016 to show GITHUB
